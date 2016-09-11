4c3e74d3831fa1121a514f6a49b06be6b94a1547
luke <baiduandsohu@gmail.com>
2016-09-11 11:36:08 +0800

    update ss mac

--- shadowsocks_sysconf/main.m
+++ shadowsocks_sysconf/main.m
@@ -74,7 +74,7 @@ int main(int argc, const char * argv[])
                         [proxies setObject:@"127.0.0.1" forKey:(NSString *)
                          kCFNetworkProxiesSOCKSProxy];
 //                        [proxies setObject:[NSNumber numberWithInteger:1080] forKey:(NSString*)
-                         kCFNetworkProxiesSOCKSPort];
+//                         kCFNetworkProxiesSOCKSPort];
                         [proxies setObject:[NSNumber numberWithInteger:10024] forKey:(NSString*)
                          kCFNetworkProxiesSOCKSPort];
                         [proxies setObject:[NSNumber numberWithInt:1] forKey:(NSString*)
